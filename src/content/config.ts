5fa6e9a4690be87bc88d39466b27bd3355ca9207
the6p4c <me@doggirl.gay>
2024-10-07 14:47:32 +0800

    use postfix .optional()

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,8 @@ import { defineCollection, z } from "astro:content";
 const postsCollection = defineCollection({
   schema: z.object({
     published: z.date(),
-    edited: z.optional(z.date()),
-    tags: z.optional(z.array(z.string())),
+    edited: z.date().optional(),
+    tags: z.array(z.string()).optional(),
   }),
 });
 
