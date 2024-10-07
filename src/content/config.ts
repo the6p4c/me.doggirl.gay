dd011c8f145eaa118f7fe1a8ba000063a18d0798
the6p4c <me@doggirl.gay>
2024-10-07 15:06:11 +0800

    add threads :)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,10 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const postsCollection = defineCollection({
   schema: z.object({
     published: z.date(),
     edited: z.date().optional(),
+    parent: reference("posts").optional(),
     tags: z.array(z.string()).optional(),
   }),
 });
