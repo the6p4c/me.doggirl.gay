235b1d1a59a204d0857ef8fb899171fe008f1b14
the6p4c <me@doggirl.gay>
2024-10-08 19:20:42 +0800

    remove settings component

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,7 @@
-import { type FormEvent } from "preact/compat";
 import { useState } from "preact/hooks";
 
 import Store from "./store";
-
-import styles from "./Editor.module.css";
-import { PostEdit } from "./Post";
+import { PostEdit } from "@components/Post";
 
 export default function Editor() {
   const [status, setStatus] = useState<string | undefined>(undefined);
@@ -42,80 +39,5 @@ export default function Editor() {
       status={status}
       onSubmit={submit}
     />
-  );
-}
-
-export function Settings() {
-  type State =
-    | { type: "success"; message: string; data?: any }
-    | { type: "error"; message: string; e?: any };
-  const [status, setState] = useState<State | null>(null);
-  const setError = (message: string, e?: any) => {
-    console.error(message, e);
-    setState({ type: "error", message, e });
-  };
-  const setSuccess = (message: string, data: any) =>
-    setState({ type: "success", message, data });
-
-  const submit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const target = e.currentTarget as typeof e.currentTarget & {
-      auth: HTMLInputElement;
-      owner: HTMLInputElement;
-      repo: HTMLInputElement;
-    };
-
-    const auth = target.auth.value;
-    const owner = target.owner.value;
-    const repo = target.repo.value;
-
-    try {
-      const store = new Store(auth, owner, repo);
-      const config = await store.getConfig();
-      setSuccess("settings saved", JSON.stringify(config));
-    } catch (e) {
-      setError("couldn't get config", e);
-    }
-  };
-
-  return (
-    <form onSubmit={submit} className={styles.form}>
-      <div className={styles.field}>
-        <label>auth</label>
-        <input name="auth" type="text" required />
-        <span className={styles.help}>your github personal access token</span>
-      </div>
-      <div className={styles.field}>
-        <label>owner</label>
-        <input name="owner" type="text" required />
-        <span className={styles.help}>your github username</span>
-      </div>
-      <div className={styles.field}>
-        <label>repo</label>
-        <input name="repo" type="text" required />
-        <span className={styles.help}>your repository's name</span>
-      </div>
-      <div className={styles.buttons}>
-        <button type="submit">save</button>
-      </div>
-      {status && (
-        <div className={styles.status}>
-          {status.type == "success" ? (
-            <>
-              <strong>success!</strong>
-              <p>{status.message}</p>
-            </>
-          ) : (
-            <>
-              <strong>something went wrong</strong>
-              <p>{status.message}</p>
-              {status.e && <pre>{"" + status.e}</pre>}
-              {status.e?.cause && <pre>{"" + status.e.cause}</pre>}
-            </>
-          )}
-        </div>
-      )}
-    </form>
   );
 }
